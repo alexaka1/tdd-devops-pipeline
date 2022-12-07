f30c46eaac803b8396e059c546060dc513b45272
T360-v4-TEDD-s03 <T360-v4-TEDD-s03@LEGUGY-06>
2022-12-07 14:25:00 +0100

    refactor

--- Proto.Web/Program.cs
+++ Proto.Web/Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+//builder.Services.AddSpaStaticFiles(configuration => configuration.RootPath = $"{GlobalWiring.AngularClient}/dist");
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+app.UseStaticFiles();
+//app.MapGet("index.html", () => "Hello World!");
+//app.usespa
 app.Run();
